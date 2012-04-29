6a2258af814cf9ee0101d995b48f1cbcaa05f8ae
James Halliday <mail@substack.net>
2012-04-28 17:39:35 -0700

    failing test for inline function toString

--- test/to_string.js
+++ test/to_string.js
@@ -1,17 +1,35 @@
 var stackedy = require('../');
 var test = require('tap').test;
 
-var src = '(' + function () {
-    function plusTen () { return x + 10 };
-    t.equal(
-        plusTen.toString().replace(/\s+/g, ''),
-        'function plusTen () { return x + 10 }'.replace(/\s+/g, '')
-    );
-} + ')()';
+var src = {
+    defun : '(' + function () {
+        function plusTen () { return x + 10 };
+        t.equal(
+            plusTen.toString().replace(/\s+/g, ''),
+            'function plusTen () { return x + 10 }'.replace(/\s+/g, '')
+        );
+    } + ')()',
+    fn : '(' + function () {
+        t.equal(
+            (function () { return x + 10 }).toString().replace(/\s+/g, ''),
+            'function () { return x + 10 }'.replace(/\s+/g, '')
+        );
+    } + ')()',
+};
 
-test('fn.toString()', function (t) {
+test('defun toString()', function (t) {
     t.plan(1);
-    var stack = stackedy(src).run({ t : t });
+    var stack = stackedy(src.defun).run({ t : t });
+    
+    stack.on('error', function (err, c) {
+        stack.stop();
+        t.fail(err);
+    });
+});
+
+test('fn toString()', function (t) {
+    t.plan(1);
+    var stack = stackedy(src.fn).run({ t : t });
     
     stack.on('error', function (err, c) {
         stack.stop();
